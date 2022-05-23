2d63d4f03341ea74e69e700ab96851def9208d22
LIZXP <peter.xli@outlook.com>
2022-05-22 19:16:37 -0600

    add emojis for visual

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = (actual, expected) => {
+  console.assert(actual === expected, `❌❌❌ ${actual} != ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion passed: 💚💚💚 ${actual} === ${expected}`);
+  }
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp")
+assertEqual(1, 1)
