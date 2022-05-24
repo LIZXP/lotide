db20428b8d1cc8a0f47526c7dfc095355c81117e
LIZXP <peter.xli@outlook.com>
2022-05-24 11:13:47 -0600

    add notes about the includes i used

--- without.js
+++ without.js
@@ -6,16 +6,18 @@
 
 const without = (arr1,arr2) => {
   let newArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (!arr2.includes(arr1[i])) {
+  for (let i=0; i<arr1.length; i++){
+    //checked if arry1 includes the value on array2
+    // then push the new !== value to the new array 
+    if (!arr2.includes(arr1[i])){
       newArr.push(arr1[i]);
     }
   }
   return newArr;
-};
+}
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
 
 console.log(words);
-console.log(without(words, ["lighthouse"]));
+console.log(without(words, ["lighthouse"]))
