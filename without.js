4cf79b4e0ebc69579389564792e3c985f3100a5f
LIZXP <peter.xli@outlook.com>
2022-05-24 11:11:51 -0600

    use loops and includes to remove the requested value from array1

---
+++ without.js
@@ -0,0 +1,21 @@
+/*
+- loop through the first array. [sloved]
+- if the 2nd array includes the same value from the first array remove it. [sloved]
+- push the remaining value into new array. [sloved]
+*/
+
+const without = (arr1,arr2) => {
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+  return newArr;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+
+console.log(words);
+console.log(without(words, ["lighthouse"]));
