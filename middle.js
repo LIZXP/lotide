5b9a1520638ef7cc27b0de5fc5d1405ecd066b82
LIZXP <peter.xli@outlook.com>
2022-05-24 16:44:28 -0600

    test the array and passed

--- middle.js
+++ middle.js
@@ -22,20 +22,24 @@ const assertArraysEqual = (arrnum1, arrnum2) => {
     console.log(`❌❌❌ Assertion failed)`);
   }
 };
+
 /*
 - determind if the length can be remined by 2, then determinds where the 2 number start and end, we need to sliced from the array.
 - we will call the variable within the else statement to prevent the pollution.
 - if not we will need to push the position of the Math.floor number of the array to the new array.
 */
 const middle = (array) => {
-  if(array.length < 2) {
+  if (array.length < 2) {
     return array;
-  } else if (array.length % 2 ===0){
-    return array.slice([array[(array.length / 2)-1], array[(array.length/2)]]);
+  } else if (array.length % 2 === 0) {
+    return array.slice(array.length / 2 - 1, array.length / 2 + 1);
   } else {
     let newArr = [];
     let num = Math.floor(array.length / 2);
     newArr.push(array[num]);
     return newArr;
   }
-}
+};
+
+middle([1,2,3,4,5]);
+middle([1,2,3,4,5,6]);
