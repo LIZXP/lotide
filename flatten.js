4b7dfc34df9af719cf7b29e077224332f040aee7
LIZXP <peter.xli@outlook.com>
2022-05-24 12:07:51 -0600

    use Array.isArray and nested loop for flatten function

---
+++ flatten.js
@@ -0,0 +1,22 @@
+/*
+  -Loop the first array [sloved]
+  -use Array.isArray to identify if array contain array [solved]
+  -push everything to new array [solved]
+*/
+
+
+const flatten = (arr1) =>{
+  let newArray = [];
+  for (let firstLoop of arr1) {
+    if (Array.isArray(firstLoop)){
+      for (sndLoop of firstLoop){
+        newArray.push(sndLoop);
+      }
+    } else {
+      newArray.push(firstLoop);
+    }
+  }
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
