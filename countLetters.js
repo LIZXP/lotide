1105bc2c5e5fc05abd896ea0de6ded00224e0dc5
LIZXP <peter.xli@outlook.com>
2022-05-26 23:08:50 -0600

    use loop and continue for the letter count

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const countLetters = (string) => {
+  let newObj = {};
+  for (let letter of string) {
+    if (letter === " ") {
+      //skip the space first using continue.
+      continue;
+    } else if (letter === letter) {
+      // loop letter is equal to loop letter.
+      if (newObj[letter]) {
+        //first run will be false [second run will + 1 when see the letter].
+        newObj[letter] += 1;
+      } else {
+        // first run will give every letter equal to 1.
+        newObj[letter] = 1;
+      }
+    }
+  }
+  return newObj;
+};
+
+console.log(countLetters("lighthouse in the house"));
