1f31d7a36b988d02e2078fef63e0c201ffd9a48a
LIZXP <peter.xli@outlook.com>
2022-05-29 22:59:59 -0600

    make some notes

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,8 @@
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   let newArray = [];
   for (let element of array) {
     if (callback(element)) {
+      // when the callback function return true, means the callback see the break point then break;
       break;
     } else {
       newArray.push(element);
