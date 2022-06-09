2574c0ebc438c39530186c7c16ea18f72df2ccd1
LIZXP <Peter.xli@outlook.com>
2022-06-08 22:59:02 -0600

    mocha chai test middle fuction

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,12 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
+const eqArrays = require("../eqArrays");
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("return 3rd item if its odd length array", () => {
+    assert.strictEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+  });
+  it("return 3rd and 4th item if its even length array", () => {
+    assert.strictEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+  });
+});
