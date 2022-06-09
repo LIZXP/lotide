5b1712cf6d0302d44b4123ba08443c88bad105be
LIZXP <Peter.xli@outlook.com>
2022-06-08 18:09:33 -0600

    refactor tail

--- tail.js
+++ tail.js
@@ -1,21 +1,7 @@
 const tail = (arr) => {
   if (arr.length > 1) {
-    for (const remain of arr.slice()) {
-      return remain;
-    }
-  }
-};
-const assertEqual = (actual, expected) => {
-  if (!actual) {
-    console.log(`Please dont use empty array.`);
-  } else {
-    console.assert(actual === expected, `❌❌❌ ${actual} != ${expected}`);
-    if (actual === expected) {
-      console.log(`Assertion passed: 💚💚💚 ${actual} === ${expected}`);
-    }
+    return arr.slice(1);
   }
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
