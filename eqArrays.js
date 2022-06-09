9bc1c5e02db19e570d639e0ccd21d1b43cf8dae4
LIZXP <Peter.xli@outlook.com>
2022-06-08 18:16:44 -0600

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,14 @@
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
-  }
-  return true;
-};
-
-
-const assertEqual = (actual, expected) => {
-  console.assert(actual === expected, `❌❌❌ ${actual} != ${expected}`);
-  if (actual === expected) {
-    console.log(`Assertion passed: 💚💚💚 ${actual} === ${expected}`);
+    return true;
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);
+module.exports = eqArrays;
